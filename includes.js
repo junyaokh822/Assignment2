4f6297ebc31272a9b323381319835bb0a0b2822c
junyaokh822 <junyaokh822@gmail.com>
2023-06-15 18:57:30 -0400

    includes done

---
+++ includes.js
@@ -0,0 +1,18 @@
+//includes() check whether an array contains a certain item
+const pets = ['dog', 'cat', 'rabbit', 'hamster'];
+
+function customIncludes(array, item) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === item) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  const hasCat = customIncludes(pets, 'cat');
+  console.log(hasCat); 
+  
+  const hasEagle = customIncludes(pets, 'eagle');
+  console.log(hasEagle); 
+  
