276fda3a373ddd2cc4cec97506a0be76f66d5d8d
junyaokh822 <junyaokh822@gmail.com>
2023-06-15 19:29:06 -0400

    lastIndexOf done

---
+++ lastIndexOf.js
@@ -0,0 +1,19 @@
+//10. lastIndexOf() -The lastIndexOf() method returns the last index at which a
+//given element can be found in the array, or -1 if it is not present. 
+//The array is searched backwards, starting at fromIndex.
+const numbers = [1, 2, 3, 4, 3, 2, 1];
+
+function customLastIndexOf(array, num) {
+    for (let i = array.length - 1; i >= 0; i--) {
+      if (array[i] === num) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+const lastIndex = customLastIndexOf(numbers, 3);
+console.log(lastIndex); // Output: 4
+
+const nonExistentIndex = customLastIndexOf(numbers, 5);
+console.log(nonExistentIndex); // Output: -1
