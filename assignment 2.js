3810598284e9477d7a176e0d6300db4a0667fa62
junyaokh822 <junyaokh822@gmail.com>
2023-06-15 13:47:49 -0400

    testing

---
+++ assignment 2.js
@@ -0,0 +1,10 @@
+const ratings = [5, 4, 5];
+let sum = 0;
+
+const sumFunction = async (a, b) => a + b;
+
+ratings.forEach(async (rating) => {
+  sum = await sumFunction(sum, rating);
+});
+
+console.log(sum);
