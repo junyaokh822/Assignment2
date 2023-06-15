a7430130448079f8ef25d7b67665ca91ff49eeef
junyaokh822 <junyaokh822@gmail.com>
2023-06-15 19:18:58 -0400

    push done

---
+++ push.js
@@ -0,0 +1,15 @@
+//9. push() - The push() method adds the specified elements 
+//to the end of an array and returns the new length of the array.
+const numbers = [1, 2, 3];
+
+function customPush(array, ...items) {
+    for (let i = 0; i < items.length; i++) {
+      array[array.length] = items[i];
+    } 
+    return array.length;
+  }  
+  
+const newLength = customPush(numbers, 4, 5, 6);
+console.log(numbers); // Output: [1, 2, 3, 4, 5, 6]
+console.log(newLength); // Output: 6
+
